3dba36b2fe768c4bdea2857701448ca2d62a61ea
kaushikTruce <kaushik@truce.io>
2026-04-17 23:00:31 +0530

    majority element II optimal TC: O(n) and SC: O(1)

--- majorityElementII.py
+++ majorityElementII.py
@@ -19,3 +19,42 @@ def majorityElement(nums):
 nums = [3, 2, 3]
 print(f"nums: {nums}")
 print(majorityElement(nums))
+
+# boyer moore algorithm
+def usingAlgo(nums):
+    candidate1, count1 = None, 0
+    candidate2, count2 = None, 0
+    resList = []
+
+    for num in nums:
+        if num == candidate1:
+            count1 += 1
+        elif num == candidate2:
+            count2 += 1
+        elif count1 == 0:
+            candidate1 = num
+            count1 += 1
+        elif count2 == 0:
+            candidate2 = num
+            count2 += 1
+        else:
+            count1 -= 1
+            count2 -= 1
+
+    freq1 = 0
+    freq2 = 0
+    for num in nums:
+        if num == candidate1:
+            freq1 += 1
+        elif num == candidate2:
+            freq2 += 1
+    
+    if freq1 > len(nums) // 3:
+        resList.append(candidate1)
+    if freq2 > len(nums) // 3:
+        resList.append(candidate2)
+
+    return resList
+
+elements = [4,2,1,1]
+print(usingAlgo(elements))
